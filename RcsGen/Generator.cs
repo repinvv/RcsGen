da85ef844fa0f04936a7541580e176e1da5f4b85
repinvv <repin.vladv@gmail.com>
2016-04-18 23:11:13 +0300

    initial

---
+++ RcsGen/Generator.cs
@@ -0,0 +1,10 @@
+﻿namespace RcsGen
+{
+    internal class Generator
+    {
+        public static string Generate(string inputFile, string nameSpace)
+        {
+            return "//text";
+        }
+    }
+}
---
+++ RcsGen/RcsGen.cs
@@ -0,0 +1,38 @@
+﻿namespace RcsGen
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using Microsoft.VisualStudio;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    [Guid("D03CB851-1B73-4447-9DD7-26FD3CFF03A9")]
+    public class RcsGen : IVsSingleFileGenerator
+    {
+        public int DefaultExtension(out string pbstrDefaultExtension)
+        {
+            pbstrDefaultExtension = ".generated.cs";
+            return VSConstants.S_OK;
+        }
+
+        public int Generate(string wszInputFilePath, 
+            string bstrInputFileContents, 
+            string wszDefaultNamespace, 
+            IntPtr[] rgbOutputFileContents, 
+            out uint pcbOutput, 
+            IVsGeneratorProgress pGenerateProgress)
+        {
+            // The contract between IVsSingleFileGenerator implementors and consumers is that 
+            // any output returned from IVsSingleFileGenerator.Generate() is returned through  
+            // memory allocated via CoTaskMemAlloc(). Therefore, we have to convert the 
+            // byte[] array returned from GenerateCode() into an unmanaged blob.  
+            var output = Generator.Generate(bstrInputFileContents, wszDefaultNamespace);
+            var bytes = Encoding.ASCII.GetBytes(output);
+            int outputLength = bytes.Length;
+            rgbOutputFileContents[0] = Marshal.AllocCoTaskMem(outputLength);
+            Marshal.Copy(bytes, 0, rgbOutputFileContents[0], outputLength);
+            pcbOutput = (uint)outputLength;
+            return VSConstants.S_OK;
+        }
+    }
+}
